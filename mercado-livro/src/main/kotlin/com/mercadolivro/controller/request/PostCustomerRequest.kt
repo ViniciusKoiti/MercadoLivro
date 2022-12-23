df8788f0274ca47e0d18cecd759e600891457762
Vinicius Koiti <viniciusnakahara@gmail.com>
2022-12-22 22:19:16 -0300

    Teste e realização de testes de Email e implementação de Swagger

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PostCustomerRequest.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/request/PostCustomerRequest.kt
@@ -1,6 +1,7 @@
 package com.mercadolivro.controller.request
 
 import com.fasterxml.jackson.annotation.JsonAlias
+import com.mercadolivro.validation.EmailAvailable
 import javax.validation.constraints.Email
 import javax.validation.constraints.NotEmpty
 
@@ -9,6 +10,7 @@ data class PostCustomerRequest(
         @field:NotEmpty( message ="Name can not be empty")
         var name:String,
         @field:Email(message = "Send a email valid please")
+        @EmailAvailable
     var email:String,
     @JsonAlias("customer_id")
     var customerId:Int
