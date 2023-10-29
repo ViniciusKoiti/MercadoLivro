ff3844aa5aa06fd4e26d7ca039dbb46fac507d4e
Vinicius Koiti <viniciusnakahara@gmail.com>
2023-10-29 13:27:25 -0300

    Mapeando Model purchase

--- mercado-livro/src/main/kotlin/com/mercadolivro/controller/PurchaseController.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/controller/PurchaseController.kt
@@ -1,15 +1,13 @@
 package com.mercadolivro.controller;
 
 
-import com.mercadolivro.controller.request.PostPurchaseRequest;
-import com.mercadolivro.controller.response.PostPurchaseResponse
+import com.mercadolivro.controller.request.PostPurchaseRequest
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("purchase")
--- mercado-livro/src/main/kotlin/com/mercadolivro/model/PurchaseModel.kt
+++ mercado-livro/src/main/kotlin/com/mercadolivro/model/PurchaseModel.kt
@@ -1,14 +1,33 @@
 package com.mercadolivro.model
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import java.math.BigDecimal
+import java.time.LocalDateTime
+import javax.persistence.*
 
 @Entity(name = "purchase")
 data class PurchaseModel(
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         var id:Int? = null,
+
+        @ManyToOne
+        @JoinColumn(name = "customer_id")
+        val customerModel: CustomerModel,
+
+        @ManyToMany
+        @JoinTable( name = "purchase_books",
+                joinColumns = [JoinColumn(name = "purchase_id")],
+                inverseJoinColumns = [JoinColumn(name = "books_id")])
+        val books: List<BookModel>,
+
+        @Column
+        val nfe: String,
+        @Column
+        val price: BigDecimal,
+
+        @Column(name = "created_at")
+        val createdAt: LocalDateTime
+
+
 ) {
 }
